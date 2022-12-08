7c640a43519967766db0da61da326e964735bda4
Alexander Mavrogiannis <alexander.mavrogiannis@and.digital>
2022-12-08 22:33:30 +0000

    more interesting app

--- Exercise_4/cont_image/flask_app/app.py
+++ Exercise_4/cont_image/flask_app/app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    user_var = os.getenv("TV_ENV")
+    user_var = os.environ.get("TV_ENV")
     return render_template('index.html', user_var=user_var)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
